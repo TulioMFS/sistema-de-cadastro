412e1e6e596d5a69bd338451cbe60852820e20a1
TulioMFS <tuliosfelix9@gmail.com>
2023-07-10 16:01:33 -0300

    Testando2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
-
 console.log("O paulo fez esse commit sem voce ver na aula!")
 console.log("Enviando para o GitHub")
 console.log("Teste1")
+console.log("Teste2")
