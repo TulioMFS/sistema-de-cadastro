14e84a38670de2ea720f5b5c45be75eb3e7e42f0
TulioMFS <tuliosfelix9@gmail.com>
2023-07-11 12:47:17 -0300

    Fazendo teste de respositorio

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("aplicação está se conectando ao banco de dados")
 
 console.log("O paulo fez esse commit sem voce ver na aula!")
 console.log("Enviando para o GitHub")
+console.log("Testando meu git")
